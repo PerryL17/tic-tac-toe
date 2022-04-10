686fa14000c3ca0069dc5e7703bcb8b9b114baf2
PerryL17 <77135000+PerryL17@users.noreply.github.com>
2022-04-10 10:22:51 -0700

    added player change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board.js";
 import "./global.css";
 
 function App() {
+  const [currentPlayer, setNextPlayer] = useState({
+    player: "Player-1",
+    letter: "X",
+  });
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Tic-Tac-Toe</h1>
-      <Board />
+      <p>
+        {currentPlayer.player} {currentPlayer.letter}
+      </p>
+      <Board currentPlayer={currentPlayer} setNextPlayer={setNextPlayer} />
     </div>
   );
 }
